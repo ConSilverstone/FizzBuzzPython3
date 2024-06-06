95c770d32d36764f45a3458a9e8671171d0746a0
ConSilverstone <40634708+ConSilverstone@users.noreply.github.com>
2024-06-06 20:04:25 +0100

    Complete Program
    Was built in Sololearn so there was no commits for this project.

--- FizzBuzzPython3.py
+++ FizzBuzzPython3.py
@@ -1,1 +1,16 @@
+## A program demonstraiting FizzBuzz ##
 
+## Initilise Variables ##
+fizz = 3 # For values that are multiple of this print fizz
+buzz = 5 # For values that are multiple of this print buzz
+
+for i in range (100):
+    if (i % fizz == 0 and i % buzz != 0): ## For values that are a multiple of fizz BUT NOT buzz
+        print ("Fizz")
+    elif (i % fizz == 0 and i % buzz != 0): ## For values that are a multiple of buzz BUT NOT fizz
+        print ("Buzz")
+    elif (i % fizz == 0 and i % buzz == 0): ## For value that are multiple of both!
+        print ("FizzBuzz")
+    else:
+        print(i) ## Otherwise just print number
+    i += 1
